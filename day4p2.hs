853f939885dccd69679b849baf88a46bbd081627
Mahesh C. Regmi <0xmahadev@gmail.com>
2025-12-05 22:46:44 +0930

    complete day 2 part 2

--- day4p2.hs
+++ day4p2.hs
@@ -33,6 +33,11 @@ getGridSize g = (x, y)
     x = length g
     y = length (head g)
 
+getGridRowFromGrid :: Grid -> Int -> GridRow
+getGridRowFromGrid [] _ = error "grid row is empty!"
+getGridRowFromGrid _ (-1) = []
+getGridRowFromGrid g i = g !! i
+
 getElementFromGridRow :: GridRow -> Int -> GridObject
 getElementFromGridRow [] _ = error "grid row is empty!"
 getElementFromGridRow r i = r !! i
@@ -51,7 +56,7 @@ getElementFromGridSafe g c r
     (cl, rl) = getGridSize g
 
 isElementSafeToMove :: Grid -> Int -> Int -> Bool
-isElementSafeToMove g c r = length (filter isEmpty [left, right, up, down, upleft, upright, downleft, downright]) > 4
+isElementSafeToMove g c r = length (filter isFilled [left, right, up, down, upleft, upright, downleft, downright]) < 4
   where
     left = getElementFromGridSafe g c (r - 1)
     right = getElementFromGridSafe g c (r + 1)
@@ -71,8 +76,58 @@ countSafeToMove g c r
     isSafe = isElementSafeToMove g c r && isFilled (getElementFromGridSafe g c r)
     (cl, rl) = getGridSize g
 
+removeAllSafeGridRow :: Grid -> Int -> Int -> GridRow
+removeAllSafeGridRow g c r
+  | r == (rl - 1) = if isSafe then [Empty] else [element]
+  | otherwise = (if isSafe then [Empty] else [element]) ++ removeAllSafeGridRow g c (r + 1)
+  where
+    isSafe = isElementSafeToMove g c r || isEmpty element
+    element = getElementFromGrid g c r
+    (cl, rl) = getGridSize g
+
+removeAllSafe' :: Grid -> Int -> Grid
+removeAllSafe' g c
+  | c >= cl = [[]]
+  | c == (cl - 1) = [removeAllSafeGridRow g c 0]
+  | otherwise = removeAllSafeGridRow g c 0 : removeAllSafe' g (c + 1)
+  where
+    (cl, _) = getGridSize g
+
+removeAllSafe g = removeAllSafe' g 0
+
+countEmpty' :: Grid -> Int -> Int -> Int
+countEmpty' g c r
+  | c >= cl = 0
+  | r >= rl = countEmpty' g (c + 1) 0
+  | otherwise = v + countEmpty' g c (r + 1)
+  where
+    v = if isEmpty element then 1 else 0
+    element = getElementFromGrid g c r
+    (cl, rl) = getGridSize g
+
+countEmpty :: Grid -> Int
+countEmpty g = countEmpty' g 0 0
+
+countRoll :: Grid -> Int
+countRoll g = totalEl - countEmpty g
+  where
+    totalEl = rl * cl
+    (rl, cl) = getGridSize g
+
+keepRemovingRolls' :: Grid -> Int -> Int
+keepRemovingRolls' g pc
+  | pc == c = c
+  | otherwise = keepRemovingRolls' safeGrid c
+  where
+    c = countRoll safeGrid
+    safeGrid = removeAllSafe g
+
+keepRemovingRolls :: Grid -> Int
+keepRemovingRolls g = keepRemovingRolls' g (countRoll g)
+
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
   let grid = readStringNewLine contents
-  print (countSafeToMove grid 0 0)
+  let lowestGrid = keepRemovingRolls grid
+  print $ countRoll grid - lowestGrid
