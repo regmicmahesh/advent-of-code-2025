987b76c1de47acef5103df978e5dac3609e86826
Mahesh C. Regmi <0xmahadev@gmail.com>
2025-12-06 15:32:12 +0930

    add day 6 solution

---
+++ day6.hs
@@ -0,0 +1,25 @@
+import Data.List (transpose)
+
+doAll :: ([Integer] -> Integer) -> [String] -> Integer
+doAll f s = f $ map (\x -> read x :: Integer) s
+
+addAll :: [String] -> Integer
+addAll = doAll sum
+
+multiplyAll :: [String] -> Integer
+multiplyAll = doAll product
+
+
+calculate :: (String, [String]) -> Integer
+calculate (op, xs)
+    | op == "+" = addAll xs
+    | op == "*" = multiplyAll xs
+    | otherwise = error "what is that operator?"
+
+
+
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+  let (ops : ns') = map words (reverse $ lines contents)
+  print $ sum $ zipWith (curry calculate) ops (transpose ns')
