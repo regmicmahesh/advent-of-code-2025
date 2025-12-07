9ffcd7b596b6fded1b9f2bc3523c88d305c52355
Mahesh C. Regmi <0xmahadev@gmail.com>
2025-12-08 03:32:04 +0930

    add solution for day 7

--- day7.hs
+++ day7.hs
@@ -14,14 +14,25 @@ calculate' _ "^" = "^"
 calculate' (x : xs) (y : ys) = c : calculate' xs ys
   where
     c | (head xs == '|') && (head ys == '^') = '|'
+      | x == '|' && y /= '^' = x
       | otherwise = y
 
 calculate a b
-    | not $ isAllCharDot b = calculate' (reverse a) (reverse (calculate' a b))
+    | not $ isAllCharDot b = reverse $ calculate' (reverse a) (reverse (calculate' a b))
     | otherwise = replace '^' '.' a
+
+
+beamkotaukocount :: String -> String -> Integer
+beamkotaukocount "^" "|" = 1
+beamkotaukocount [x] [y] = 0
+beamkotaukocount (x:xs) (y:ys) = (if (x == '|') && (y == '^') then 1 else 0) + beamkotaukocount xs ys
 
 main = do
   contents <- readFile "input.txt"
   let l = map (replace 'S' '|') $ lines contents
-  let z = foldl calculate (head l) (take 9 l)
+  let z = map (\x -> foldl1 calculate (take x l)) [1..(length l)]
   print z
+  let x = zip (init z) (tail z)
+  let d = map (uncurry beamkotaukocount) x
+  print $ sum d
+
